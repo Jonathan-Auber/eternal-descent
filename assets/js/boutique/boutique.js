e22ea0b48390684d7778dbcc3212916e572bad6b
Jonathan.Auber <auber.jonathan@gmail.com>
2023-02-22 10:26:07 +0100

    Compteur js ok! Début du travail sur le popup

--- assets/js/boutique/boutique.js
+++ assets/js/boutique/boutique.js
@@ -1,1 +1,19 @@
 const counter = document.getElementById("count");
+const addMugBlanc = document.querySelector("#add-mug-blanc");
+const addMugNoir = document.getElementById("add-mug-noir");
+const addTShirtBlanc = document.getElementById("add-tshirt-blanc");
+const addTShirtNoir = document.getElementById("add-tshirt-noir");
+const addPoster = document.getElementById("add-poster");
+const addPeluche = document.getElementById("add-peluche");
+
+
+function addToCart (goodie) {
+counter.innerHTML++;
+}
+
+addMugBlanc.addEventListener("click", addToCart);
+addMugNoir.addEventListener("click", addToCart);
+addTShirtBlanc.addEventListener("click", addToCart);
+addTShirtNoir.addEventListener("click", addToCart);
+addPoster.addEventListener("click", addToCart);
+addPeluche.addEventListener("click", addToCart);
