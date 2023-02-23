38c0fdec69c8110e95f6de4712e86eb4e85de865
Jonathan.Auber <auber.jonathan@gmail.com>
2023-02-23 10:55:49 +0100

    Javascript du panier fonctionel

--- assets/js/boutique/boutique.js
+++ assets/js/boutique/boutique.js
@@ -42,7 +42,7 @@ const totalPrice = document.querySelector("#total-price");
 
 function isDeleveryFree() {
     // Penser à modifier le total
-    totalPrice.innerHTML = parseInt(mugBlancTotalPrice.innerHTML) + parseInt(mugNoirTotalPrice.innerHTML);
+    totalPrice.innerHTML = parseInt(mugBlancTotalPrice.innerHTML) + parseInt(mugNoirTotalPrice.innerHTML) + parseInt(tShirtBlancTotalPrice.innerHTML) + parseInt(tShirtNoirTotalPrice.innerHTML) + parseInt(posterTotalPrice.innerHTML) + parseInt(pelucheTotalPrice.innerHTML);
     if (totalPrice.innerHTML >= 50) {
         deliveryPrice.innerHTML = "Offert"
     } else {
@@ -52,28 +52,6 @@ function isDeleveryFree() {
 
 // Controle du panier
 const counter = document.getElementById("count");
-
-
-
-const addTShirtBlanc = document.getElementById("add-tshirt-blanc");
-const tShirtBlanc = document.querySelector(".mn-tab");
-const tShirtBlancTotal = document.querySelector("#mn-count");
-const tShirtBlancTotalPrice = document.querySelector("#mn-price");
-
-const addTShirtNoir = document.getElementById("add-tshirt-noir");
-const tShirtNoir = document.querySelector(".tsn-tab");
-const tShirtNoirTotal = document.querySelector("#tsn-count");
-const tShirtNoirTotalPrice = document.querySelector("#tsn-price");
-
-const addPoster = document.getElementById("add-poster");
-const poster = document.querySelector(".poster-tab");
-const posterTotal = document.querySelector("#poster-count");
-const posterTotalPrice = document.querySelector("#poster-price");
-
-const addPeluche = document.getElementById("add-peluche");
-const peluche = document.querySelector(".peluche-tab");
-const pelucheTotal = document.querySelector("#peluche-count");
-const pelucheTotalPrice = document.querySelector("#peluche-price");
 
 // Article Mug Blanc
 const addMugBlanc = document.querySelector("#add-mug-blanc");
@@ -139,6 +117,7 @@ function addToCartMugNoir() {
 }
 
 addMugNoir.addEventListener("click", addToCartMugNoir);
+
 // Fonction ajout ou retrait d'article dans le panier 
 const mugNoirAdd = document.querySelector(".btn-plus.mn");
 const mugNoirRemove = document.querySelector(".btn-moins.mn");
@@ -161,15 +140,204 @@ function addMN() {
     isDeleveryFree();
 }
 
-
 addMugNoir.addEventListener("click", addToCartMugNoir);
 mugNoirAdd.addEventListener("click", addMN);
 mugNoirRemove.addEventListener("click", removeMN);
 
+// Article T-Shirt Blanc 
+const addTShirtBlanc = document.getElementById("add-tshirt-blanc");
+const tShirtBlanc = document.querySelector(".tsb-tab");
+const tShirtBlancTotal = document.querySelector("#tsb-count");
+const tShirtBlancTotalPrice = document.querySelector("#tsb-price");
 
-addMugNoir.addEventListener("click", addToCart);
-addTShirtBlanc.addEventListener("click", addToCart);
-addTShirtNoir.addEventListener("click", addToCart);
-addPoster.addEventListener("click", addToCart);
-addPeluche.addEventListener("click", addToCart);
+function addToCartTShirtBlanc() {
+    if (tShirtBlanc.classList !== "tsb-tab") {
+        tShirtBlanc.classList.remove("off");
+    }
+    tShirtBlancTotal.innerHTML++;
+    tShirtBlancTotalPrice.innerHTML = tShirtBlancTotal.innerHTML * 35;
+    counter.innerHTML++;
+    isDeleveryFree();
 
+}
+
+addTShirtBlanc.addEventListener("click", addToCartTShirtBlanc);
+// Fonction ajout ou retrait d'article dans le panier 
+const tShirtBlancAdd = document.querySelector(".btn-plus.tsb");
+const tShirtBlancRemove = document.querySelector(".btn-moins.tsb");
+
+function removeTSB() {
+    tShirtBlancTotal.innerHTML--;
+    tShirtBlancTotalPrice.innerHTML = tShirtBlancTotal.innerHTML * 35;
+    counter.innerHTML--;
+    isDeleveryFree();
+    if (tShirtBlancTotal.innerHTML == 0) {
+        tShirtBlanc.classList.add("off");
+    }
+
+}
+
+function addTSB() {
+    tShirtBlancTotal.innerHTML++; 35
+    tShirtBlancTotalPrice.innerHTML = tShirtBlancTotal.innerHTML * 35;
+    counter.innerHTML++;
+    isDeleveryFree();
+}
+
+addTShirtBlanc.addEventListener("click", addToCartTShirtBlanc);
+tShirtBlancAdd.addEventListener("click", addTSB);
+tShirtBlancRemove.addEventListener("click", removeTSB);
+
+// Article T-Shirt Noir
+const addTShirtNoir = document.getElementById("add-tshirt-noir");
+const tShirtNoir = document.querySelector(".tsn-tab");
+const tShirtNoirTotal = document.querySelector("#tsn-count");
+const tShirtNoirTotalPrice = document.querySelector("#tsn-price");
+
+function addToCartTShirtNoir() {
+    if (tShirtNoir.classList !== "tsn-tab") {
+        tShirtNoir.classList.remove("off");
+    }
+    tShirtNoirTotal.innerHTML++;
+    tShirtNoirTotalPrice.innerHTML = tShirtNoirTotal.innerHTML * 35;
+    counter.innerHTML++;
+    isDeleveryFree();
+
+}
+
+addTShirtNoir.addEventListener("click", addToCartTShirtNoir);
+// Fonction ajout ou retrait d'article dans le panier 
+const tShirtNoirAdd = document.querySelector(".btn-plus.tsn");
+const tShirtNoirRemove = document.querySelector(".btn-moins.tsn");
+
+function removeTSN() {
+    tShirtNoirTotal.innerHTML--;
+    tShirtNoirTotalPrice.innerHTML = tShirtNoirTotal.innerHTML * 35;
+    counter.innerHTML--;
+    isDeleveryFree();
+    if (tShirtNoirTotal.innerHTML == 0) {
+        tShirtNoir.classList.add("off");
+    }
+
+}
+
+function addTSN() {
+    tShirtNoirTotal.innerHTML++;
+    tShirtNoirTotalPrice.innerHTML = tShirtNoirTotal.innerHTML * 35;
+    counter.innerHTML++;
+    isDeleveryFree();
+}
+
+addTShirtNoir.addEventListener("click", addToCartTShirtNoir);
+tShirtNoirAdd.addEventListener("click", addTSN);
+tShirtNoirRemove.addEventListener("click", removeTSN);
+
+// Article Poster
+const addPoster = document.getElementById("add-poster");
+const poster = document.querySelector(".poster-tab");
+const posterTotal = document.querySelector("#poster-count");
+const posterTotalPrice = document.querySelector("#poster-price");
+
+console.log(addPoster)
+console.log(poster)
+console.log(posterTotal)
+console.log(posterTotalPrice)
+
+function addToCartPoster() {
+    if (poster.classList !== "poster-tab") {
+        poster.classList.remove("off");
+    }
+    posterTotal.innerHTML++;
+    posterTotalPrice.innerHTML = posterTotal.innerHTML * 60;
+    counter.innerHTML++;
+    isDeleveryFree();
+
+}
+
+addPoster.addEventListener("click", addToCartPoster);
+
+// Fonction ajout ou retrait d'article dans le panier 
+const posterAdd = document.querySelector(".btn-plus.poster");
+const posterRemove = document.querySelector(".btn-moins.poster");
+
+function removePo() {
+    posterTotal.innerHTML--;
+    posterTotalPrice.innerHTML = posterTotal.innerHTML * 60;
+    counter.innerHTML--;
+    isDeleveryFree();
+    if (posterTotal.innerHTML == 0) {
+        poster.classList.add("off");
+    }
+
+}
+
+function addPo() {
+    posterTotal.innerHTML++;
+    posterTotalPrice.innerHTML = posterTotal.innerHTML * 60;
+    counter.innerHTML++;
+    isDeleveryFree();
+}
+
+addPoster.addEventListener("click", addToCartPoster);
+posterAdd.addEventListener("click", addPo);
+posterRemove.addEventListener("click", removePo);
+
+// Article Peluche
+const addPeluche = document.getElementById("add-peluche");
+const peluche = document.querySelector(".peluche-tab");
+const pelucheTotal = document.querySelector("#peluche-count");
+const pelucheTotalPrice = document.querySelector("#peluche-price");
+
+function addToCartPeluche() {
+    if (peluche.classList !== "peluche-tab") {
+        peluche.classList.remove("off");
+    }
+    pelucheTotal.innerHTML++;
+    pelucheTotalPrice.innerHTML = pelucheTotal.innerHTML * 40;
+    counter.innerHTML++;
+    isDeleveryFree();
+
+}
+
+addPeluche.addEventListener("click", addToCartPeluche);
+
+// Fonction ajout ou retrait d'article dans le panier 
+const pelucheAdd = document.querySelector(".btn-plus.peluche");
+const pelucheRemove = document.querySelector(".btn-moins.peluche");
+
+function removePel() {
+    pelucheTotal.innerHTML--;
+    pelucheTotalPrice.innerHTML = pelucheTotal.innerHTML * 40;
+    counter.innerHTML--;
+    isDeleveryFree();
+    if (pelucheTotal.innerHTML == 0) {
+        peluche.classList.add("off");
+    }
+
+}
+
+function addPel() {
+    pelucheTotal.innerHTML++;
+    pelucheTotalPrice.innerHTML = pelucheTotal.innerHTML * 40;
+    counter.innerHTML++;
+    isDeleveryFree();
+}
+
+addPeluche.addEventListener("click", addToCartPeluche);
+pelucheAdd.addEventListener("click", addPel);
+pelucheRemove.addEventListener("click", removePel);
+
+// Fonction pour vider le panier
+const viderPanier = document.querySelector("#btn-delete");
+const allGoodies = document.querySelectorAll(".hide");
+const allGoodiesReset = document.querySelectorAll(".reset");
+
+
+function deleteCart() {
+    counter.innerHTML = 0;
+    allGoodiesReset.forEach(element => element.innerHTML = 0)
+    allGoodies.forEach(element => element.classList.add("off"));
+    isDeleveryFree();
+}
+
+viderPanier.addEventListener('click', deleteCart);
